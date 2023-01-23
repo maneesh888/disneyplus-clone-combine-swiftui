75620a09f204baed1748e2140ae18f1265448a6f
Maneesh M <maneesh888@gmail.com>
2023-01-23 19:10:18 +0400

    Project setup completed

---
+++ DisneyPlusApp/DisneyPlusApp/DisneyPlusAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DisneyPlusAppApp.swift
+//  DisneyPlusApp
+//
+//  Created by Maneesh M on 23/01/23.
+//
+
+import SwiftUI
+
+@main
+struct DisneyPlusAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
