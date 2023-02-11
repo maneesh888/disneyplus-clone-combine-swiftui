52842a523faf8d445ba4b91d525689f1db4aed99
Maneesh M <maneesh888@gmail.com>
2023-02-11 10:52:59 +0400

    Profile and Search category model added

---
+++ DisneyPlusApp/DisneyPlusApp/Models/SearchCategory.swift
@@ -0,0 +1,16 @@
+//
+//  SearchCategory.swift
+//  DisneyPlusApp
+//
+//  Created by Maneesh M on 11/02/23.
+//
+
+import Foundation
+
+struct SearchCategory: Identifiable {
+    let id = UUID()
+    var title: String
+    var systemImage: String
+}
+
+
