17b8e3e91ebba7f8f511ccd37bc0a886deb816c1
Maneesh M <maneesh888@gmail.com>
2023-02-11 10:46:31 +0400

    Added channel model

---
+++ DisneyPlusApp/DisneyPlusApp/Models/Channel.swift
@@ -0,0 +1,12 @@
+//
+//  Channel.swift
+//  DisneyPlusApp
+//
+//  Created by Maneesh M on 11/02/23.
+//
+
+import Foundation
+
+enum Channel: String, CaseIterable {
+    case disney, pixar, marvel, sw, ng
+}
